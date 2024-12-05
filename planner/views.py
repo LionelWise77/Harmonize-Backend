9a8b70d7fe939af73ed57e69cbb2b0076d8ca7c9
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-12-05 20:58:52 +0000

    created serializers, views, urls plannerAPP

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# Vista para listar y crear tareas
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Asigna el usuario autenticado como propietario de la tarea
+        serializer.save(owner=self.request.user)
+
+# Vista para obtener, actualizar o eliminar una tarea individual
+class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
