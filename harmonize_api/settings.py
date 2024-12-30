2198e2bdb81cd34470fd9acd8fed5cabf9a98b33
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-12-30 09:09:09 +0000

    changing CORS

--- harmonize_api/settings.py
+++ harmonize_api/settings.py
@@ -123,8 +123,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-     os.environ.get('CLIENT_ORIGIN', 'http://localhost:8000'), 
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.codeinstitute-ide\.net$"
 ]
 
 
